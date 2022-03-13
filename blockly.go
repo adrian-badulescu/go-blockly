dabce715a36aee182176398011ac6f2b37a0b0d0
Adrian Badulescu <adrianb2104@gmail.com>
2022-03-13 16:31:02 +0200

    gopath path added

--- blockly.go
+++ blockly.go
@@ -2,12 +2,12 @@ package blockly
 
 import (
 	"net/http"
-
+	"os"
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterBlocklyAutomationEndpoint(c *gin.Engine) {
 	// router.Load
 	// c.HTML(http.StatusOK, "index.html", nil)
-	c.StaticFS("/BlocklyAutomation", http.Dir("./blockly"))
+	c.StaticFS("/BlocklyAutomation", http.Dir(os.Getenv("GOPATH")))
 }
