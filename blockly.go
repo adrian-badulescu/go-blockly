dcd259c920deae302babc96afe08240ae4bbb999
Adrian Badulescu <adrian.badulescu@nn.ro>
2022-04-11 19:12:17 +0300

    test

--- blockly.go
+++ blockly.go
@@ -1,8 +1,10 @@
-package blockly
+package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
+	"path/filepath"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,4 +12,12 @@ func RegisterBlocklyAutomationEndpoint(c *gin.Engine) {
 	// router.Load
 	// c.HTML(http.StatusOK, "index.html", nil)
 	c.StaticFS("/BlocklyAutomation", http.Dir(os.Getenv("GOPATH")))
+	
+	fmt.Println("a:", (http.Dir(filepath.Dir(os.Getenv("GOPATH")))), filepath.Dir("/pkg/mod/github.com/adrian-badulescu/go-blockly@v0.0.0-20220313143102-dabce715a36a"))
+}
+
+
+func main() {
+	router := gin.Default()
+	RegisterBlocklyAutomationEndpoint(router)
 }
