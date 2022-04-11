65f6cf6ce614caa9b540ada3ce2e91cc78fb4ea0
Adrian Badulescu <adrianb2104@gmail.com>
2022-04-11 22:14:40 +0300

    test2

--- blockly.go
+++ blockly.go
@@ -15,14 +15,18 @@ func RegisterBlocklyAutomationEndpoint(c *gin.Engine) {
 	c.StaticFS("/BlocklyAutomation", http.Dir(os.Getenv("GOPATH")))
 
 	c.StaticFS("/BlocklyAutomationa", http.Dir("."))
-	c.StaticFS("/BlocklyAutomationb", http.Dir(os.Getenv("GOPATH")+"/pkg/mod/github.com/adrian-badulescu/go-blockly@v0.0.0-20220411180535-5dfd966c0cd0"))
+	c.StaticFS("/BlocklyAutomationb", http.Dir("/home/adi/go/pkg/mod/github.com/adrian-badulescu/go-blockly@v0.0.0-20220411180535-5dfd966c0cd0"))
 
 	fmt.Println(http.Dir(os.Getenv("GOPATH")))
 
 	// fmt.Println("a:", (http.Dir(filepath.Dir(os.Getenv("GOPATH")))), filepath.Dir("/pkg/mod/github.com/adrian-badulescu/go-blockly@v0.0.0-20220313143102-dabce715a36a"))
+	fmt.Println(http.Dir(os.Getenv("GOPATH") + "/pkg/mod/github.com/adrian-badulescu/go-blockly@v0.0.0-20220411180535-5dfd966c0cd0"))
 }
 
 // func main() {
-// 	router := gin.Default()
-// 	RegisterBlocklyAutomationEndpoint(router)
+// 	// router := gin.Default()
+// 	// RegisterBlocklyAutomationEndpoint(router)
+// 	fmt.Println(http.Dir(os.Getenv("GOPATH") + "/pkg/mod/github.com/adrian-badulescu/go-blockly@v0.0.0-20220411180535-5dfd966c0cd0"))
+// 	fmt.Println(os.Getenv("GOPATH"))
+
 // }
